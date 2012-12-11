5f4c4f4ec3101bb81f7bdc0453da7206e6dfb231
Hakan Ensari <hakan.ensari@papercavalier.com>
2012-12-11 17:53:33 +0000

    Remove location scope

--- js/app.js
+++ js/app.js
@@ -44,40 +44,35 @@ $(function() {
       return response.photos.photo;
     },
   }, {
-    // Searches Flickr for a phrase, scoping query by browser's location.
+    // Searches Flickr for a phrase.
     //
     // phrase   - A search phrase.
     // callback - A function which is called with a random image.
     search: function(phrase, callback) {
-      (new Location).fetch({
-        success: function(location) {
-          var flickr = new Flickr,
-              lat    = location.get('latitude'),
-              lon    = location.get('longitude');
+      var text = phrase
+        .replace(/[^ \w]/g, '')
+        .toLowerCase()
+        .split(' ')
+        .sort(function(a, b) {
+          return b.toString().length - a.toString().length;
+        })
+        .shift();
 
-          var query = phrase
-            .split(' ')
-            .sort(function(a, b) {
-              return b.toString().length - a.toString().length;
-            })
-            .splice(0, 1)
-            .join();
+      var flickr = new this;
 
-          flickr.url = '//api.flickr.com/services/rest/?jsoncallback=?&' +
-            $.param({
-              api_key: "22e9685a8bef1253e96ffd92ebc6c676",
-              format:  "json",
-              method:  "flickr.photos.search",
-              sort:    "interestingness-desc",
-              text:    query
-            });
+      flickr.url = '//api.flickr.com/services/rest/?jsoncallback=?&' +
+        $.param({
+          api_key: "22e9685a8bef1253e96ffd92ebc6c676",
+          format:  "json",
+          method:  "flickr.photos.search",
+          sort:    "interestingness-desc",
+          text:    text
+        });
 
-          flickr.fetch({
-            success: function(flickr) {
-              var index = Math.floor(Math.random() * flickr.models.length);
-              callback(flickr.models[index]);
-            }
-          });
+      flickr.fetch({
+        success: function(flickr) {
+          var index = Math.floor(Math.random() * flickr.models.length);
+          callback(flickr.models[index]);
         }
       });
     },
