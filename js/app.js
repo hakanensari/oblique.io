6ed502ec8722e8dc59db15c8f30cecad1715cc6f
Hakan Ensari <hakanensari@dot.local>
2012-12-11 16:59:23 +0000

    For fun, scope Flickr search by location

--- js/app.js
+++ js/app.js
@@ -3,71 +3,98 @@ try {
 } catch(e) {}
 
 $(function() {
-  // This is a random oblique strategy.
+  // The browser's location.
+  var Location = Backbone.Model.extend({
+    url: 'http://freegeoip.net/json/?callback=?'
+  });
+
+  // A random strategy.
   var Strategy = Backbone.Model.extend({
-    url: '//api.oblique.io?jsoncallback=?',
-  });
-
-  // Fetch some random images from Flickr.
-  var Image = Backbone.Model.extend({
-    src: function() {
-      return "//farm"             + this.get('farm') +
-             ".staticflickr.com/" + this.get('server') +
-             "/"                  + this.get('id') +
-             "_"                  + this.get('secret') +
-             ".jpg"
-    }
-  });
-
-  var Images = Backbone.Collection.extend({
-    model: Image,
-
-    initialize: function(phrase) {
-      var sortByLength = function(a, b) {
-        return(b.toString().length - a.toString().length);
-      }
-
-      var query = phrase
-        .split(' ')
-        .sort(sortByLength)
-        .splice(0, 1)
-        .join()
-
-      this.url = '//api.flickr.com/services/rest/?jsoncallback=?&' +
-        $.param({
-          api_key:      "22e9685a8bef1253e96ffd92ebc6c676",
-          content_type: 1,
-          format:       "json",
-          method:       "flickr.photos.search",
-          sort:         "interestingness-desc",
-          text:         query
-        });
-    },
+    url: '//api.oblique.io?callback=?',
 
     parse: function(response) {
-      return(response.photos.photo);
-    },
-
-    sample: function() {
-      return this.models[Math.floor(Math.random() * this.models.length)]
+      return { phrase: response }
     }
   });
 
-  var strategy = new Strategy;
+  // A Flickr image.
+  var Image = Backbone.Model.extend({
+    // Returns the URL of the image.
+    //
+    // size - An optional size (m, s, t, z, or b).
+    url: function(size) {
+      var host = 'farm' + this.get('farm') + '.staticflickr.com',
+          path = '/' + this.get('server') + '/';
 
-  strategy.fetch({
+      var filename = this.get('id') + '_' + this.get('secret');
+      if(size) {
+        filename += '_' + size;
+      }
+      filename += '.jpg';
+
+      return '//' + host + path + filename;
+    }
+  });
+
+  // A crude wrapper around Flickr photo search.
+  var Flickr = Backbone.Collection.extend({
+    model: Image,
+
+    parse: function(response) {
+      return response.photos.photo;
+    },
+  }, {
+    // Searches Flickr for a phrase, scoping query by browser's location.
+    //
+    // phrase -   A search phrase.
+    // callback - A function which is called with a random image.
+    search: function(phrase, callback) {
+      (new Location).fetch({
+        success: function(location) {
+          var flickr = new Flickr,
+              lat    = location.get('latitude'),
+              lon    = location.get('longitude');
+
+          var query = phrase
+            .split(' ')
+            .sort(function(a, b) {
+              return b.toString().length - a.toString().length;
+            })
+            .splice(0, 1)
+            .join();
+
+          flickr.url = '//api.flickr.com/services/rest/?jsoncallback=?&' +
+            $.param({
+              api_key: "22e9685a8bef1253e96ffd92ebc6c676",
+              format:  "json",
+              method:  "flickr.photos.search",
+              sort:    "interestingness-desc",
+              text:    query
+            });
+
+          flickr.fetch({
+            success: function(flickr) {
+              var index = Math.floor(Math.random() * flickr.models.length);
+              callback(flickr.models[index]);
+            }
+          });
+        }
+      });
+    },
+  });
+
+  // Fetch a random strategy.
+  (new Strategy).fetch({
     success: function(strategy, _, _) {
       var $container = $('.container');
       $container.html('<p>' + strategy.get('phrase') + '</p>');
 
-      var images = new Images(strategy.get('phrase'));
-      images.fetch({
-        success: function(images, _, _) {
-          $('.cover')
-            .css({
-              'background-image': 'url(' + images.sample().src() + ')',
-            })
-        }
+      // Add a random background image.
+      Flickr.search(strategy.get('phrase'), function(image) {
+        $('.cover')
+          .css({
+            'background-image': 'url(' + image.url() + ')',
+          })
       });
     }
   });
